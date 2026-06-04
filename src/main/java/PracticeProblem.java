32172a0840bfea9b72251f9ef5a6df9d35d76152
SalmaDanane <343249595@tdsb.ca>
2026-06-04 14:39:52 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -7,7 +7,7 @@ public class PracticeProblem {
  
  public static boolean isOdd(int num) {
      
-     return num % 2 !0;
+     return (num % 2) != 0;
  }
  
 public static boolean isPositive(int num){
@@ -21,11 +21,11 @@ public static boolean isNegative(int num){
     return num < 0;
 }
 
- public static in combinedLength(String word, String word2){
+ public static boolean combinedLength(String word, String word2){
+     
+
      
      return word.length() + word2.length();
  }
     
 }
-
-
